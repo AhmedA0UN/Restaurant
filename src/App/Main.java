d7a7b23bd53cb01a9904c440b994467392fe0e71
Ahmed <ahmed.aoun@isimg.tn>
2026-05-11 23:43:25 +0100

    ajouter un dossier App contenant la fichier principale Main.java

--- src/App/Main.java
+++ src/App/Main.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package App;
 
 import view.Choisir;
@@ -13,19 +12,3 @@ public class Main {
         });
     }
 }
-=======
-package App;
-
-import view.Choisir;
-import javax.swing.SwingUtilities;
-
-public class Main {
-    public static void main(String[] args) {
-        // Launch the GUI on the Event Dispatch Thread
-        SwingUtilities.invokeLater(() -> {
-            Choisir frame = new Choisir();
-            frame.setVisible(true);
-        });
-    }
-}
->>>>>>> 768dd945fcf6c608e553d73c03c152ad3f966253
